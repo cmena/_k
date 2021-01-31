f24af4420066ec6d1a11ae01d365ceff9b83ec84
cesar mena <cesar.mena@gmail.com>
2021-01-30 16:04:26 -0800

    configurable profiles

--- profile.py
+++ profile.py
@@ -1,1 +1,7 @@
-etc/profile.py
+def load(path):
+    """load the user's profile by evaling the contents of path in the profile.py
+    namespace.
+
+    """
+    with open(path) as infile:
+        exec(infile.read(), globals())
